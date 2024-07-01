9e98f6b1de34ebf27ce87d9b0838c99bcf9fdccc
BenjGit <courtin-benjamin@outlook.fr>
2024-07-01 21:09:24 +0200

    final dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Dashboard from './pages/Dashboard'
-import { Layout } from './components/Layout';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import "./index.css";
+import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Dashboard />,
       },
     ],
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
